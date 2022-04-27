1795fd60839132f39b99d740174611d123283b66
ChubyChad69 <104509609+klub07@users.noreply.github.com>
2022-04-27 10:46:25 -0400

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-# Name: [Your Name]
-# Date: 4/24/2022
-# Project: Countdown Timer using Github
-# File: countdownClass --> main.py
-#-----------------------------------------------------------------
